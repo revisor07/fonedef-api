3c042da63a5b67650f5bd0d456f11a5d139530de
Yuriy <revisor14@gmail.com>
2020-05-18 14:19:14 -0700

    api keys setup

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express();
 
 const domainRoutes = require('./api/routes/domains')
 
+app.use(function(req, res, next) {
+      if ((req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+     } else
+        next();
+    });
+
 app.use('/domains', domainRoutes);
+
+
 
 module.exports = app;
